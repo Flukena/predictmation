0bf24415617698cad6c4e37fe0786a61a833e323
Flukena <flukgoza@gmail.com>
2021-04-28 16:54:54 +0700

    set

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+<<<<<<< Updated upstream
 const path = require('path')
 const moment = require('moment')
+=======
+const path = require('path');
+const moment = require('moment');
+const cookie = require('cookie-session');
+const cors = require('cors')
+>>>>>>> Stashed changes
 const app = express();
 
+app.use(cors())
 
 const logger = (req, res, next)=>{
     console.log(`${req.protocol}://${req.get('host')} ${req.originalUrl}: ${moment().format()}`)
 }
 app.use(logger)
+
+app.use(cookie({
+    name:'session',
+    keys:['key1', 'key2'],
+    maxAge: 3600 * 1000
+}))
 
 app.use(express.static('static'))
 app.use(express.json()) // for parsing application/json
