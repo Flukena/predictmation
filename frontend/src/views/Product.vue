5f793c49722dc292e805556109906ffa1a816bd2
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-04-29 22:27:59 +0700

    update

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -1,32 +1,119 @@
 <template>
-  <div class="container is-fullhd">
+  <div class="main">
     <!-- <div class="pro_img"></div>-->
-    <div class="main">
     <div class="columns is-desktop">
-      <div class="column" >
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg" width="50px" height="80px" alt="">
-            </figure>
+        <div class="column is-3">
+          <div class="card" id="go">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
+                  width="50px"
+                  height="80px"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">ตัวอย่าง</div>
+            </div>
+            <div id="butpro">
+              <button class="button is-success is-inverted">Buy</button>
+            </div>
           </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div><div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
         </div>
-      </div>
-      
-    </div>
-    </div>
-    
+
+        <div class="column is-3">
+          <div class="card" id="go">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
+                  width="50px"
+                  height="80px"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">ตัวอย่าง</div>
+            </div>
+            <div id="butpro">
+              <button class="button is-success is-inverted">Buy</button>
+            </div>
+          </div>
+        </div>
+        <div class="column is-3">
+          <div class="card" id="go">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
+                  width="50px"
+                  height="80px"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">ตัวอย่าง</div>
+            </div>
+            <div id="butpro">
+              <button class="button is-success is-inverted">Buy</button>
+            </div>
+          </div>
+        </div>
+        <div class="column is-3">
+          <div class="card" id="go">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
+                  width="50px"
+                  height="80px"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">ตัวอย่าง</div>
+            </div>
+            <div id="butpro">
+              <button class="button is-success is-inverted">Buy</button>
+            </div>
+          </div>
+        </div>
+        
+
+    </div> 
+    <div class="columns is-desktop">
+      <div class="column is-3">
+          <div class="card" id="go">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
+                  width="50px"
+                  height="80px"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">ตัวอย่าง</div>
+            </div>
+            <div id="butpro">
+              <button class="button is-success is-inverted">Buy</button>
+            </div>
+          </div>
+        </div>
+      </div> 
+
     <!-- <div class="Baskets">
       <a class="button is-rounded is-medium"
         ><i class="fas fa-shopping-basket fa-1x mr-2"></i>
         <span> Baskets</span></a
       >
     </div> -->
-    
   </div>
 </template>
