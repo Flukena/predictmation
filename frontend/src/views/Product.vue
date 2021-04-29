d5d2d6a9274568a6c4133f493e574a90ee0dc2b6
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-04-29 20:53:56 +0700

    Kut

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -1,9 +1,85 @@
 <template>
   <div class="container is-fullhd">
     <!-- <div class="pro_img"></div>-->
+    <div class="main">
+    <div class="columns is-desktop">
+      <div class="column" >
+        <div class="card" id="go">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                url=""
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">werewr</div>
+          </div>
+        </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
+      </div>
+      <div class="column">
+        <div class="card" id="go">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">werewr</div>
+          </div>
+        </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
+      </div>
+      <div class="column">
+        <div class="card" id="go">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">werewr</div>
+          </div>
+        </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
+      </div>
+      <div class="column">
+        <div class="card" id="go">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">werewr</div>
+          </div>
+        </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
+      </div>
+      
+    </div>
     <div class="columns is-desktop">
       <div class="column">
-        <div class="card">
+        <div class="card" id="go">
           <div class="card-image">
             <figure class="image is-4by3">
               <img
@@ -13,33 +89,15 @@
             </figure>
           </div>
           <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-              <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+            <div class="content">werewr</div>
           </div>
         </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
       </div>
       <div class="column">
-        <div class="card">
+        <div class="card" id="go">
           <div class="card-image">
             <figure class="image is-4by3">
               <img
@@ -49,33 +107,15 @@
             </figure>
           </div>
           <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-              <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+            <div class="content">werewr</div>
           </div>
         </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
       </div>
       <div class="column">
-        <div class="card">
+        <div class="card" id="go">
           <div class="card-image">
             <figure class="image is-4by3">
               <img
@@ -85,33 +125,15 @@
             </figure>
           </div>
           <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-              <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+            <div class="content">werewr</div>
           </div>
         </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
       </div>
       <div class="column">
-        <div class="card">
+        <div class="card" id="go">
           <div class="card-image">
             <figure class="image is-4by3">
               <img
@@ -121,37 +143,21 @@
             </figure>
           </div>
           <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-              <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+            <div class="content">werewr</div>
           </div>
         </div>
+        <div id="butpro">
+        <button class="button is-success is-inverted" >Buy</button>
+      </div>
       </div>
     </div>
-    <div class="Baskets">
+    </div>
+
+    <!-- <div class="Baskets">
       <a class="button is-rounded is-medium"
         ><i class="fas fa-shopping-basket fa-1x mr-2"></i>
         <span> Baskets</span></a
       >
-    </div>
+    </div> -->
   </div>
 </template>
