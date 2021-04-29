f8b615d4de76a647c58cd4780726e5772553e301
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-04-29 10:00:42 +0700

    Update

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,10 +1,10 @@
-<template>
+
+  <template>
   <div class="column is-2 is-offset-5">
-    <form
-      class="box"
-      style="background-color: rgb(246, 228, 211); padding: 1.6rem"
-    >
-      <label for="">For customers</label>
+    <form class="box">
+      <label for="" class="for">For customers</label><br>
+      <div class="column is-2"></div>
+      
       <div class="field">
         <div class="control">
           <input class="input" type="email" placeholder="Username" />
@@ -16,11 +16,15 @@
           <input class="input" type="password" placeholder="Password" />
         </div>
       </div>
+      
       <label class="checkbox">
         <input type="checkbox" />
         Keep me signed in
       </label>
+      <div class="column is-2"></div>
+      
       <button class="button is-black" style="width: 100%">Log in</button>
     </form>
   </div>
+  
 </template>
