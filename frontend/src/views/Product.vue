ba457f7b99ac4c457433ab3f1f8a9e79a3af59da
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-04-29 21:40:59 +0700

    Kuttee

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -7,10 +7,7 @@
         <div class="card" id="go">
           <div class="card-image">
             <figure class="image is-4by3">
-              <img
-                url=""
-                alt="Placeholder image"
-              />
+              <img src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg" width="50px" height="80px" alt="">
             </figure>
           </div>
           <div class="card-content">
@@ -19,147 +16,17 @@
         <button class="button is-success is-inverted" >Buy</button>
       </div>
         </div>
-        
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div>
-          <div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div>
-          <div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div><div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
       </div>
       
     </div>
-    <div class="columns is-desktop">
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div><div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div><div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div> <div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-       
-      </div>
-      <div class="column">
-        <div class="card" id="go">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">werewr</div>
-          </div><div id="butpro">
-        <button class="button is-success is-inverted" >Buy</button>
-      </div>
-        </div>
-        
-      </div>
     </div>
-    </div>
-
+    
     <!-- <div class="Baskets">
       <a class="button is-rounded is-medium"
         ><i class="fas fa-shopping-basket fa-1x mr-2"></i>
         <span> Baskets</span></a
       >
     </div> -->
+    
   </div>
 </template>
