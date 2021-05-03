3c60a0d98d1f0264ee38b5228718f479ee9cc4bf
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-05-03 16:35:06 +0700

    Update

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -33,12 +33,12 @@
         <div class="navbar-end">
           <div class="navbar-item">
 
-            <div class="buttons"><router-link to="/user/signup">
-              <a class="button  is-rounded is-medium" style="background:#9A5648; color:white">
+            <div class="buttons" ><router-link to="/user/signup">
+              <a class="button  is-rounded is-medium" id="singlog">
                 
                 <strong>Sign up</strong>
               </a></router-link><router-link to="/user/login">
-              <a class="button is-light is-rounded is-medium">
+              <a class="button  is-rounded is-medium" id="singlog2"  style="margin-left: 15px">
 
                 Log in
               </a></router-link>
