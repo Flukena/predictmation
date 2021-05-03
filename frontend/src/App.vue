91a9d1b59939c74d784e51591a63eb2cdc355a63
Flukena <flukgoza@gmail.com>
2021-05-03 17:45:49 +0700

    tag srcipt

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app" style="height:-100px">
-    <!-- <Reset></Reset> -->
-    <!-- <Styles></Styles> -->
     <Navbar></Navbar>
     <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
-    <!-- <Footer></Footer> -->
     </div>
 </template>
 
 <script>
-// import Index from './views/Index';
 import '../css/style.css'
 import '../css/reset.css'
 import Navbar from './components/Navbar'; 
-// import Footer from './components/Footer'; 
-// import User from './views/User'
 import axios from '@/plugins/axios'
 export default {
   name: 'App',
   components: {
-    // Reset,
-    // Styles,
     Navbar,
-    // Footer,
   },data(){
     return{
       user:null
