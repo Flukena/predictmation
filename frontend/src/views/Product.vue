87b82b810ea2f0a7791c02f302fbba82fd6126fa
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-05-03 23:10:47 +0700

    Update 3/5/2021 23:10

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -3,23 +3,24 @@
     <!-- <div class="pro_img"></div>-->
     <div class="columns is-desktop">
       <div class="column is-3">
-        <div class="card" id="go">
+        <div class="card" id="go"  style="border-radius: 10%" >
           <div class="card-image">
             <figure class="image is-4by3">
-              <img
+              <img style="border-top-left-radius: 10%; border-top-right-radius: 10%" 
                 src="https://acuisineth.com/app/uploads/2020/07/Apple-Cinnamon-Cake-12-1024x683.jpg"
                 alt=""
               />
             </figure>
           </div>
           <div class="card-content">
-            <div class="content">ตัวอย่าง</div>
+            <div class="content" style="margin-bottom: -50%">Example</div>
           </div>
           <div id="butpro">
-            <button class="button is-success is-inverted">Buy</button>
+            <button class="button is-success is-inverted" style="font-weight: 600;" >Buy</button>
           </div>
         </div>
       </div>
+      
     </div>
 
     <!-- <div class="Baskets">
