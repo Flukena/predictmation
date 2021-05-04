181723cc71b7c5d3c44e62b211cdbba72d48c79e
Flukena <flukgoza@gmail.com>
2021-05-05 00:25:38 +0700

    update

---
+++ backend/routes/product.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { router } = require(".");
+const pool = require("../config");
+const {isLoggedIn} = require('../middlewares');
+router.express.Router();
+
+router.get("/product", async  (req, res, next)=>{
+    
+} )
