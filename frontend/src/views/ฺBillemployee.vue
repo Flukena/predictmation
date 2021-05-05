1c8c2cdf1fb8b4fb162e9360418ef4058cf3e6de
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-05-05 10:53:27 +0700

    Update

--- frontend/src/views/ฺBillemployee.vue
+++ frontend/src/views/ฺBillemployee.vue
@@ -1,15 +1,79 @@
 <template>
   <div class="column is-2 is-offset-5">
     <form class="boxbill" style="height: 60%;">
-      <div class="columns">
+      <div class="columns" style="background-color: black;">
         <div class="column">
-          
+          <h1 id="billemtitlestyle">Order list</h1>
         </div>
 
         <div class="column">
-          
+          <h1 id="billemtitlestyle">Price (฿)</h1>
+        </div>
+         <div class="column">
+          <h1 id="billemtitlestyle">Date</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemtitlestyle">Time</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemtitlestyle">Paid</h1>
+        </div>
+      </div><br>
+      <div class="columns">
+        <div class="column">
+          <h1 id="billemstyle">Late</h1>
+        </div>
+
+        <div class="column">
+          <h1 id="billemstyle">15</h1>
+        </div>
+         <div class="column">
+          <h1 id="billemstyle">3/4/2564</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">10:50</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">Yes</h1>
+        </div>
+      </div><br>
+      <div class="columns">
+        <div class="column">
+          <h1 id="billemstyle">Green tea</h1>
+        </div>
+
+        <div class="column">
+          <h1 id="billemstyle">20</h1>
+        </div>
+         <div class="column">
+          <h1 id="billemstyle">27/4/2564</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">23:15</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">No</h1>
+        </div>
+      </div><br>
+      <div class="columns">
+        <div class="column">
+          <h1 id="billemstyle">Mocca</h1>
+        </div>
+
+        <div class="column">
+          <h1 id="billemstyle">20</h1>
+        </div>
+         <div class="column">
+          <h1 id="billemstyle">1/5/2564</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">12:45</h1>
+        </div>
+        <div class="column">
+          <h1 id="billemstyle">Yes</h1>
         </div>
       </div>
+      
       
     </form>
     <div>
