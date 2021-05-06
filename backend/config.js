12f396211c00a824f5ee233356bfcd3de4bd239e
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2021-05-06 23:36:49 +0700

    Update

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'password',
-  database: 'SQL_cafe',
+  password: '172352',
+  database: 'webpro',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,6 +1,6 @@
 
   <template>
-  <div class="column is-2 is-offset-5" style="height:600px" id="loginwall">
+  <div class="column is-2 is-offset-5" style="height: 600px" id="loginwall">
     <form class="box">
       <div class="columns">
         <div class="column">
@@ -14,26 +14,42 @@
               <input
                 class="input"
                 type="username"
-                v-model="username"
+                v-model="$v.username.$model"
+                :class="{ 'is-danger': $v.username.$error }"
                 placeholder="Username"
               />
             </div>
+            <template v-if="$v.username.$error">
+              <p class="help is-danger" id="help" v-if="!$v.username.$complex3">
+                * At least 5 characters.
+              </p>
+            </template>
           </div>
 
           <div class="field" style="width: 100%">
             <div class="control">
               <input
                 class="input"
+                id="myInput"
                 type="password"
-                v-model="password"
+                v-model="$v.password.$model"
+                :class="{ 'is-danger': $v.password.$error }"
                 placeholder="Password"
               />
             </div>
+            <template v-if="$v.password.$error">
+              <p class="help is-danger" id="help" v-if="!$v.password.$complex">
+                * At least 8 characters.
+              </p>
+              <p class="help is-danger" id="help" v-if="!$v.password.$complex2">
+                * Contain lowercase, uppercase and numbers.
+              </p>
+            </template>
           </div>
 
           <label class="checkbox">
-            <input type="checkbox" />
-            Keep me signed in
+            <input type="checkbox" @click="myFunction()" />
+            Show password
           </label>
           <div class="column is-2"></div>
 
@@ -48,7 +64,8 @@
               display: block;
               margin-left: auto;
               margin-right: auto;
-              height: 250px;"
+              height: 250px;
+            "
             src="http://www.clker.com/cliparts/J/C/X/z/E/s/shopping-cart-hi.png"
           />
         </div>
@@ -57,15 +74,51 @@
   </div>
 </template>
 <script>
+import { required, minLength } from "vuelidate/lib/validators";
 import axios from "@/plugins/axios";
+
 export default {
   data() {
     return {
       username: "",
       password: "",
-      error: "",
     };
   },
+  validations: {
+    username: {
+      required,
+      minLength: minLength(1),
+      complex3(value) {
+        if (value.length < 5) {
+          return false;
+        }
+        return true;
+      },
+    },
+    password: {
+      required,
+      minLength: minLength(8),
+      complex(value) {
+        if (value.length < 8) {
+          return false;
+        }
+        return true;
+      },
+      complex2(value) {
+        if (
+          !(
+            value.match(/[a-z]/) &&
+            value.match(/[A-Z]/) &&
+            value.match(/[0-9]/)
+          )
+        ) {
+          return false;
+        }
+        return true;
+      },
+    },
+  },
+
   methods: {
     submit() {
       const data = {
@@ -78,13 +131,22 @@ export default {
           const token = res.data.token;
           localStorage.setItem("token", token);
           this.$emit("auth-change");
-          this.$router.push({path:"/" });
+          this.$router.push({ path: "/" });
         })
         .catch((error) => {
-           this.error = error
-           console.log(error)
+          this.error = error;
+          console.log(error);
         });
     },
-  },
+    myFunction() {
+      var x = document.getElementById("myInput");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+
+  }
 };
 </script>
